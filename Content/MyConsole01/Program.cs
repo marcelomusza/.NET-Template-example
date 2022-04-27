322d0f09a2080bf882f1c079841cbabf9f6f0dc7
Marcelo Muszalski <marcelo.muszalski@dcsl.com>
2022-04-27 12:46:01 +0200

    Add project files.

---
+++ Content/MyConsole01/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, MyConsole01!");
+
+double[] array = new double[] { 1.2, 3, 2 };
+var sum = 0.0;
+if (array.Any())
+    foreach (var item in array)
+        sum += item;
+else
+    
+
+Console.WriteLine(sum);
